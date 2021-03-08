7fe50152df940c9933092dd30223e3ddcf3bc639
Dmytro Anikin <dimianni@users.noreply.github.com>
2021-03-08 12:49:05 +0100

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,36 @@
+
+
+var container = document.querySelector(".container");
+var mask = document.querySelector(".mask");
+var blur = document.querySelector(".blur");
+var maskContent = document.querySelector(".mask-content");
+
+window.addEventListener("load", onMove);
+
+container.addEventListener("mousemove", onMove);
+
+function onMove(e) {
+
+    var x = e.pageX - 150;
+    var y = e.pageY - 150;
+
+    gsap.set(mask, {
+        left: x,
+        top: y
+    });
+
+    gsap.set(blur, {
+        x: x,
+        y: y
+    });
+
+    // Картинка с контентом идет в противоположном направлении мышке, поэтому создается ощущение что она стоит на месте
+
+    gsap.set(maskContent, {
+        left: -x,
+        top: -y
+    });
+}
+
+
+
